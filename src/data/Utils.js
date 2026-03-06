2298198fd139f75822b1accb091f1c466c33dfb1
carlex mandou <carlexmandou@gmail.com>
2026-03-06 18:05:50 +0100

    Add a system to choose different sources for deferent hosting services for the exam series papers

--- src/data/Utils.js
+++ src/data/Utils.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
+const HOST = process.env.HOST;
 
 exports.readFile = function(){
     try {
-        const data = JSON.parse(fs.readFileSync("./src/data/exams.json", "utf8"));
+        const data = JSON.parse(fs.readFileSync(path.join(__dirname,`./exams-${HOST}.json`)), "utf8");
         return data;
     } catch (err) {
         throw err;
